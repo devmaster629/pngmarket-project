ac84ebd6ec2b2b84bb65ae08647d7a6e23efd60e
devmaster629 <rentalmagnet.dev@gmail.com>
2026-09-15 11:38:54 -0300

    account page and footer update

--- oc-content/themes/epsilon_child/functions_child.php
+++ oc-content/themes/epsilon_child/functions_child.php
@@ -7,7 +7,7 @@
  */
 
 if (!defined('PNGM_CHILD_VERSION')) {
-    define('PNGM_CHILD_VERSION', '2.5.18');
+    define('PNGM_CHILD_VERSION', '2.5.19');
 }
 
 require_once dirname(__FILE__) . '/includes/vehicle_makes.php';
