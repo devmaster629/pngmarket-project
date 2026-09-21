c21f6657957cfe1c3f9d3a876ccc25524247cb91
devmaster629 <rentalmagnet.dev@gmail.com>
2026-09-21 16:47:12 -0300

    remove blank on the posting for mobile

--- oc-content/themes/epsilon_child/functions_child.php
+++ oc-content/themes/epsilon_child/functions_child.php
@@ -7,7 +7,7 @@
  */
 
 if (!defined('PNGM_CHILD_VERSION')) {
-    define('PNGM_CHILD_VERSION', '2.7.0');
+    define('PNGM_CHILD_VERSION', '2.7.1');
 }
 
 require_once dirname(__FILE__) . '/includes/vehicle_makes.php';
