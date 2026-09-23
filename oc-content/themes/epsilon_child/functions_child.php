007b39d8b1266cdec27c2c0257e041758551db66
devmaster629 <rentalmagnet.dev@gmail.com>
2026-09-23 01:37:55 -0300

    description on the listing details

--- oc-content/themes/epsilon_child/functions_child.php
+++ oc-content/themes/epsilon_child/functions_child.php
@@ -7,7 +7,7 @@
  */
 
 if (!defined('PNGM_CHILD_VERSION')) {
-    define('PNGM_CHILD_VERSION', '2.9.5');
+    define('PNGM_CHILD_VERSION', '2.9.6');
 }
 
 /** Minimum password length for registration / password change (complexity is advisory only). */
