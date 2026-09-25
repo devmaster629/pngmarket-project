1e0d02f48cbb83e8d6eee4a6610895d8aa8f2946
devmaster629 <rentalmagnet.dev@gmail.com>
2026-09-25 11:11:55 -0300

    social login in one line

--- oc-content/themes/epsilon_child/functions_child.php
+++ oc-content/themes/epsilon_child/functions_child.php
@@ -7,7 +7,7 @@
  */
 
 if (!defined('PNGM_CHILD_VERSION')) {
-    define('PNGM_CHILD_VERSION', '2.9.16');
+    define('PNGM_CHILD_VERSION', '2.9.17');
 }
 
 /** Minimum password length for registration / password change (complexity is advisory only). */
